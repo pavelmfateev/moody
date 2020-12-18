d7efe23be5e88c1ba287400d6b35c3fa1e5ed772
Pavel Fateev <pavel.m.fateev@gmail.com>
2020-12-17 21:40:17 -0800

    install body-parser

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const app = express()
 const port = 3000
 
 app.use(express.static(__dirname + "/public"));
+app.set("view engine", "ejs");
 
 app.get('/', (req, res) => {
-  res.render('index.ejs');
+  res.render('index');
 })
 
 app.listen(port, () => {
