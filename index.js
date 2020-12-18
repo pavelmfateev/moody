dfeabe08fa0fc2280c4d47c2e118ec82bdfe0a40
Pavel Fateev <pavel.m.fateev@gmail.com>
2020-12-18 12:33:36 -0800

    install mongoose

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({ extended: false }))
 app.set("view engine", "ejs");
 
 app.get('/', (req, res) => {
+  res.render('index');
+})
+
+app.post('/track', (req, res) => {
   res.render('index');
 })
 
