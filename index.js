ba9cfb4293a756fdf4565bd05e936e97b385a865
Pavel Fateev <pavel.m.fateev@gmail.com>
2020-12-19 14:44:07 -0800

    add checklist

--- index.js
+++ index.js
@@ -2,15 +2,19 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const path = require("path");
-const bodyParser = require("body-parser");
 
 app.use(express.static(path.join(__dirname, "public")));
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   res.render("index");
+});
+
+app.post('/checklist', (req, res) => {
+  console.log(req.body);
+  res.render('checklist');
 });
 
 // app.get('/r/:name/:id', (req, res) => {
