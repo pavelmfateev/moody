27fe8309c7b65fb33d70d8af0f52863892abccd0
Pavel Fateev <pavel.m.fateev@gmail.com>
2020-12-24 13:21:22 -0800

    add uuid

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// SETUP
 const express = require("express");
 const app = express();
 const port = 3000;
@@ -8,36 +9,45 @@ app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
+// Temporary test data
+let questions = [
+  "Feeling sad or down in the dumps",
+  "Feeling unhappy or blue",
+  "Crying spells or tearfulness",
+  "Feeling discouraged"
+]
+
+let checklists = [
+  {
+    id:,
+    date:new Date(),
+    comment:,
+    1: 0,
+    2: 1, 
+    3: 2,
+    4: 3 
+
+  }
+]
+
+// ROUTES
 app.get('/', (req, res) => {
   res.render("index");
 });
 
-app.get('/checklist', (req, res) => {
-  const questions = [
-    "Feeling sad or down in the dumps",
-    "Feeling unhappy or blue",
-    "Crying spells or tearfulness",
-    "Feeling discouraged"
-  ]
-
-  res.render('checklist', {question : questions});
+app.get('/checklists', (req, res) => {
+  res.render('checklists');
 });
 
-app.post('/checklist', (req, res) => {
+app.post('/checklists', (req, res) => {
   console.log(req.body);
+  
   res.send("Hello");
 });
 
-// app.get('/r/:name/:id', (req, res) => {
-//   const {name, id} = req.params;
-//   console.log(req.params);
-//   res.send(`<h1>Browsing the ${name} with id: ${id}</h1>`)
-// });
-
-// app.get('/search', (req, res) => {
-//   const {q, color} = req.query;
-//   res.send(`<h1>Browsing the ${color} ${q}</h1>`)
-// });
+app.get('/checklists/new', (req, res) => {
+  res.render('new', {questions});
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
