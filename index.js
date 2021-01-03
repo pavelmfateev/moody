672c487f584096c2d638e23cf0e3f506b2b3c7d3
Pavel Fateev <pavel.m.fateev@gmail.com>
2021-01-02 18:10:16 -0800

    connect to mongodb locally

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ const path = require("path");
 const methodOverride = require("method-override");
 const { v4: uuid } = require("uuid");
 
+const mongoose = require("mongoose");
+mongoose
+  .connect("mongodb://localhost:27017/depressionApp", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("CONNECTION OPEN");
+  })
+  .catch((err) => {
+    console.log("OH NO ERROR");
+    console.log(err);
+  });
+
 app.use(express.static(path.join(__dirname, "public")));
 //To parse form data in POST request body:
 app.use(express.urlencoded({ extended: true }));
@@ -23,6 +37,27 @@ let questions = [
   "Feeling unhappy or blue",
   "Crying spells or tearfulness",
   "Feeling discouraged",
+  "Feeling hopeless",
+  "Low self-esteem",
+  "Feeling worthless or inadequate",
+  "Guilt or shame",
+  "Criticizing yourself or blaming others",
+  "Difficulty making decisions",
+  "Loss of interest in family, friends or colleagues",
+  "Loneliness",
+  "Spending less time with family or friends",
+  "Loss of motivation",
+  "Loss of interest in work or other activities",
+  "Avoiding work or other activities",
+  "Loss of pleasure or satisfaction in life",
+  "Feeling tired",
+  "Difficulty sleeping or sleeping too much",
+  "Decreased or increased appetite",
+  "Loss of interest in sex",
+  "Worrying about your health",
+  "Do you have any suicidal thoughts?",
+  "Would you like to end your life?",
+  "Do you have a plan for harming yourself?",
 ];
 
 const dateGen = () => {
@@ -135,11 +170,11 @@ app.patch("/checklists/:id", (req, res) => {
 // *******************************************
 // DELETE/DESTROY- removes a single comment
 // *******************************************
-app.delete('/checklists/:id', (req, res) => {
+app.delete("/checklists/:id", (req, res) => {
   const { id } = req.params;
-  checklists = checklists.filter(c => c.id !== id);
-  res.redirect('/checklists');
-})
+  checklists = checklists.filter((c) => c.id !== id);
+  res.redirect("/checklists");
+});
 
 // HELPER METHODS
 // converts json response values from string to numbers
@@ -174,6 +209,76 @@ const checklistFill = (arr, total, comment) => {
 
   return checklist;
 };
+
+// MONGO SCHEMAS
+const checklistSchema = new mongoose.Schema({
+  answers: [
+    {
+      q1: Number,
+      q2: Number,
+      q3: Number,
+      q4: Number,
+      q5: Number,
+      q6: Number,
+      q7: Number,
+      q8: Number,
+      q9: Number,
+      q10: Number,
+      q11: Number,
+      q12: Number,
+      q14: Number,
+      q15: Number,
+      q16: Number,
+      q17: Number,
+      q18: Number,
+      q19: Number,
+      q20: Number,
+      q21: Number,
+      q22: Number,
+      q23: Number,
+      q24: Number,
+      q25: Number,
+    },
+  ],
+  total: Number,
+  dateAdded: String,
+  comment: String,
+});
+
+const Checklist = mongoose.model("Checklist", checklistSchema);
+// const firstTest = new Checklist({
+//   answers: [
+//     {
+//       q1: 0,
+//       q2: 1,
+//       q3: 2,
+//       q4: 3,
+//       q5: 0,
+//       q6: 1,
+//       q7: 2,
+//       q8: 3,
+//       q9: 2,
+//       q10: 3,
+//       q11: 1,
+//       q12: 1,
+//       q14: 0,
+//       q15: 0,
+//       q16: 0,
+//       q17: 1,
+//       q18: 2,
+//       q19: 3,
+//       q20: 3,
+//       q21: 2,
+//       q22: 1,
+//       q23: 0,
+//       q24: 1,
+//       q25: 0,
+//     },
+//   ],
+//   total: 25,
+//   dateAdded: dateGen(),
+//   comment: "Did this work?",
+// });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
