3e2009ac9b0e5c31e304f0734eef15f925f93dd8
Pavel Fateev <pavel.m.fateev@gmail.com>
2021-02-07 15:28:27 -0800

    add layout boilerplate (ejs-mate)

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const express = require("express"),
   port = 3000,
   path = require("path"),
   methodOverride = require("method-override"),
-  checklistsRoutes = require("./routes/checklists.js");
+  checklistsRoutes = require("./routes/checklists.js"),
+  mongoose = require("mongoose"),
+  ejsMate = require('ejs-mate');
 
-const mongoose = require("mongoose");
 mongoose.set("useFindAndModify", false);
 mongoose
   .connect("mongodb://localhost:27017/depressionApp", {
@@ -29,6 +30,7 @@ app.use(express.json());
 // To 'fake' put/patch/delete requests:
 app.use(methodOverride("_method"));
 // Views folder and EJS setup:
+app.engine('ejs', ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
